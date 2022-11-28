55562e81c75274b0e46e8a1c36312dda4e33c198
Diego Gonzalez <diegonzalezv93@gmail.com>
2022-11-27 23:53:02 -0300

    change about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                     </div>
                     <div></div>
                 </div>
-                <div className='max-w-[100px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
                         <p>
                             Hi. I'm Diego, nice to meet you. Please take a look
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
             {/*Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600 '>Hi, my name is </p>
-                <h className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
                     Diego N. Gonzalez
-                </h>
+                </h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
                     I'm a FrontEnd Developer
                 </h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
-                    I’m a full-stack developer specializing in building (and
+                    I’m a front-end developer specializing in building (and
                     occasionally designing) exceptional digital experiences.
                     Currently, I’m focused on building responsive full-stack web
                     applications.
